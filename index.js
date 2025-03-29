cbad8c8d445da4a98d55924020239340bb4a9cd5
Alex5ander <alexsandergutierreez@gmail.com>
2025-03-28 23:16:12 -0300

    update

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ const io = new Server({ cors: { origin: '*' }, parser });
 
 const PORT = process.env.PORT || 3000;
 
+/** @type { {[key:string]: Room}} */
+let rooms = {};
+
+const isOverlap = (a, b) => {
+  return hypot((a.x + a.h / 2) - (b.x + b.h / 2), a.y + a.h / 2 - (b.y + b.h / 2)) < (a.h + b.h) / 2;
+}
+
+/** @param {Player} player  */
+const checkBounds = (player) => {
+  if (player.x + player.h > 640) {
+    player.x = 640 - player.h;
+  }
+  if (player.x < 0) {
+    player.x = 0;
+  } if (player.y + player.h > 480) {
+    player.y = 480 - player.h;
+  } if (player.y < 0) {
+    player.y = 0;
+  }
+}
+
 const tanks = [
   sheet.tankBeige,
   sheet.tankBlack,
@@ -51,28 +72,30 @@ class WorldObject {
   }
 }
 
-class Smoke {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.angle = random() * PI * 2;
-    this.sizes = [
-      sheet.smokeGrey3,
-      sheet.smokeGrey2,
-      sheet.smokeGrey1
-    ].map(e => ({ ...e, width: e.width * scale, height: e.height * scale }))
-  }
-}
+// class Smoke {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//     this.angle = random() * PI * 2;
+//     this.sizes = [
+//       sheet.smokeGrey3,
+//       sheet.smokeGrey2,
+//       sheet.smokeGrey1
+//     ].map(e => ({ ...e, width: e.width * scale, height: e.height * scale }))
+//   }
+// }
 
 class Bullet {
-  constructor(x, y, angle, owner) {
-    this.x = x;
-    this.y = y;
+  constructor() {
+    this.x = 0;
+    this.y = 0;
     this.w = bullet.width * scale;
     this.h = bullet.height * scale;
-    this.angle = angle;
+    this.angle = 0;
     this.speed = 15;
-    this.owner = owner;
+    this.id = randomUUID();
+    this.ownerId = '';
+    this.roomId = '';
     this.disable = false;
   }
   update() {
@@ -81,14 +104,21 @@ class Bullet {
     let m = abs(hypot(320 - this.x, 240 - this.y));
     if (m > 1000) {
       this.disable = true;
+      delete rooms[this.roomId].shots[this.id];
+      io.to(this.roomId).emit('removeBullet', this.id);
     }
   }
 }
 
 class Player {
-  constructor(id) {
-    let index = floor(random() * tanks.length);
+  /** 
+   * @param {string} id  
+   * @param {string} room
+  */
+  constructor(id, roomId) {
     this.id = id;
+    this.roomId = roomId;
+    let index = floor(random() * tanks.length);
     this.x = floor(random() * (640 - tanks[index].width));
     this.y = floor(random() * (480 - tanks[index].height));
     this.w = tanks[index].width * scale;
@@ -101,30 +131,88 @@ class Player {
     this.speed = 5;
     this.rotatespeed = 5;
     this.shotDelay = 750;
-    this.life = 1;
-    this.keys = [];
+    this.life = 100;
     this.shotLastTime = 0;
     this.barrel = { sprite: index, w: barrels[index].width * scale, h: barrels[index].height * scale, angle: 0 };
     this.deadTime = 0;
+  }
+  checkCollisions() {
+    return rooms[this.roomId].objects.some(t => isOverlap(this, t) || Object.values(rooms[this.roomId].players).filter(t => t != this).some(t => isOverlap(this, t)));
+  }
+  move(keys) {
+    let actions = {
+      KeyW: () => {
+        this.x += sin(this.angle) * this.speed;
+        this.y -= cos(this.angle) * this.speed;
+        const result = checkBounds(this) || this.checkCollisions();
+        if (result) {
+          this.x -= sin(this.angle) * this.speed;
+          this.y += cos(this.angle) * this.speed;
+        }
+      },
+      KeyS: () => {
+        this.x -= sin(this.angle) * this.speed;
+        this.y += cos(this.angle) * this.speed;
+        const result = checkBounds(this) || this.checkCollisions();
+        if (result) {
+          this.x += sin(this.angle) * this.speed;
+          this.y -= cos(this.angle) * this.speed;
+        }
+      },
+      KeyA: () => {
+        this.angle -= this.rotatespeed * PI / 180;
+      },
+      KeyD: () => {
+        this.angle += this.rotatespeed * PI / 180;
+      },
+      KeyZ: () => {
+        this.barrel.angle -= this.rotatespeed * PI / 180;
+      },
+      KeyX: () => {
+        this.barrel.angle += this.rotatespeed * PI / 180;
+      },
+      Space: () => {
+        if (Date.now() - this.shotLastTime > this.shotDelay) {
+          let bangle = this.barrel.angle + this.angle;
+          let bullet = new Bullet();
+          bullet.x = (this.x + this.w / 2) - bullet.w / 2 + sin(bangle) * this.barrel.h;
+          bullet.y = (this.y + this.h / 2) - bullet.h / 2 - cos(bangle) * this.barrel.h;
+          bullet.angle = bangle;
+          bullet.ownerId = this.id;
+          bullet.roomId = this.roomId;
+          rooms[this.roomId].shots[bullet.id] = bullet;
+          this.shotLastTime = Date.now();
+          io.to(this.roomId).emit('createBullet', bullet);
+        }
+      }
+    }
+    for (const key of keys) {
+      if (actions[key]) {
+        actions[key]();
+        io.to(this.roomId).emit('playerMoved', this);
+      }
+    }
   }
   respawn() {
     this.x = floor(random() * (640 - this.w));
     this.y = floor(random() * (480 - this.h));
     this.angle = Math.random() * PI * 2;
-    this.destroyed = false;
-    this.life = 1;
+    this.barrel.angle = 0;
+    while (this.checkCollisions()) {
+      this.respawn();
+    }
+    this.life = 100;
+    io.to(this.roomId).emit('respawn', this);
   }
 }
 
 class Room {
   constructor() {
     this.id = randomUUID();
-    /** @type {Player[]} */
-    this.players = [];
-    /** @type {Bullet[]} */
-    this.shots = [];
-    /** @type {Smoke[]} */
-    this.smokes = [];
+    /** @type {{[key:string]:Player}} */
+    this.players = {};
+    /** @type {{[key:string]:Bullet}} */
+    this.shots = {};
     /** @type {Tree[]} */
     this.objects = [];
     this.terrain = floor(random() * 3);
@@ -132,185 +220,90 @@ class Room {
       this.objects.push(new WorldObject(random() * 640, random() * 480, floor(random() * objects.length)))
     }
   }
-  /** @param {Player} player  */
-  checkBounds(player) {
-    if (player.x + player.h > 640) {
-      player.x = 640 - player.h;
-    }
-    if (player.x < 0) {
-      player.x = 0;
-    } if (player.y + player.h > 480) {
-      player.y = 480 - player.h;
-    } if (player.y < 0) {
-      player.y = 0;
-    }
-  }
-  isOverlap(a, b) {
-    return hypot((a.x + a.h / 2) - (b.x + b.h / 2), a.y + a.h / 2 - (b.y + b.h / 2)) < (a.h + b.h) / 2;
-  }
-  checkCollisions(player) {
-    return this.objects.some(t => this.isOverlap(player, t) || this.players.filter(t => t != player).some(t => this.isOverlap(player, t)));
-  }
   update() {
-    let actions = {
-      KeyW: (player) => {
-        player.x += sin(player.angle) * player.speed;
-        player.y -= cos(player.angle) * player.speed;
-        this.checkBounds(player);
-        if (this.checkCollisions(player)) {
-          player.x -= sin(player.angle) * player.speed;
-          player.y += cos(player.angle) * player.speed;
-        }
-      },
-      KeyS: (player) => {
-        player.x -= sin(player.angle) * player.speed;
-        player.y += cos(player.angle) * player.speed;
-        this.checkBounds(player);
-        if (this.checkCollisions(player)) {
-          player.x += sin(player.angle) * player.speed;
-          player.y -= cos(player.angle) * player.speed;
-        }
-      },
-      KeyA: (player) => {
-        player.angle -= player.rotatespeed * PI / 180;
-      },
-      KeyD: (player) => {
-        player.angle += player.rotatespeed * PI / 180;
-      },
-      KeyZ: (player) => {
-        player.barrel.angle -= player.rotatespeed * PI / 180;
-      },
-      KeyX: (player) => {
-        player.barrel.angle += player.rotatespeed * PI / 180;
-      },
-      Space: (player) => {
-        if (Date.now() - player.shotLastTime > player.shotDelay) {
-          let bangle = player.barrel.angle + player.angle;
-          let bullet = new Bullet();
-          bullet.x = (player.x + player.w / 2) - bullet.w / 2 + sin(bangle) * player.barrel.h;
-          bullet.y = (player.y + player.h / 2) - bullet.h / 2 - cos(bangle) * player.barrel.h;
-          bullet.angle = bangle;
-          bullet.owner = player;
-          this.shots.push(bullet);
-          this.smokes.push(new Smoke(bullet.x + bullet.w / 2, bullet.y + bullet.h / 2));
-          player.shotLastTime = Date.now();
-        }
+    for (const id in this.players) {
+      const player = this.players[id];
+      if (Date.now() - player.deadTime > 3000 && player.life <= 0) {
+        player.respawn();
       }
     }
 
-    this.players.forEach(player => {
-      if (!player.destroyed) {
-        player.keys.forEach(key => { actions[key]?.(player) })
-      }
-      player.keys = [];
-    });
-
-    this.shots.forEach(shot => {
+    for (const shotId in this.shots) {
+      const shot = this.shots[shotId];
       shot.update();
-
-      if (!shot.disable) {
-        this.players.find(player => {
-          if (shot.owner != player && !player.destroyed && player.x + player.w > shot.x && player.x < shot.x + shot.w && player.y + player.h > shot.y && player.y < shot.y + shot.h) {
-            player.life -= 0.25;
-            if (player.life <= 0) {
-              shot.owner.kills += 1;
-              player.life = 0;
-              player.destroyed = true;
-              player.deadTime = Date.now();
-              let randomSize = 5 + floor(random() * 10);
-              for (let i = 0; i < randomSize; i++) {
-                let x = (player.x + player.w / 2) + floor(10 - random() * 10);
-                let y = (player.y + player.h / 2) + floor(10 - random() * 10);
-                this.smokes.push(new Smoke(x, y));
-              }
-            }
-            shot.disable = true;
-            this.smokes.push(new Smoke(shot.x - shot.w / 2, shot.y - shot.h / 2));
-          }
-        });
-
-        this.objects.forEach(o => {
-          if (o.x + o.w > shot.x && o.x < shot.x + shot.w && o.y + o.h > shot.y && o.y < shot.y + shot.h) {
-            shot.disable = true;
-            this.smokes.push(new Smoke(shot.x - shot.w / 2, shot.y - shot.h / 2));
-          }
-        })
+      if (this.objects.some(o => isOverlap(shot, o))) {
+        delete this.shots[shot.id];
+        io.to(this.id).emit('removeBullet', shot.id);
       }
-    });
-
-    this.shots = this.shots.filter(shot => !shot.disable);
-
-    if (this.players.some(e => e.destroyed)) {
-      this.players.filter(e => e.destroyed && Date.now() - e.deadTime >= 3000).forEach(e => {
-
-        e.respawn();
-        while (this.checkCollisions(e)) {
-          e.respawn();
+      if (shot) {
+        for (const id in this.players) {
+          const player = this.players[id];
+          if (player.life > 0 && player.id != shot.ownerId) {
+            if (isOverlap(shot, player)) {
+              delete this.shots[shot.id];
+              player.life -= 10;
+              if (player.life <= 0) {
+                player.deadTime = Date.now();
+                this.players[shot.ownerId].kills += 1;
+              }
+              io.to(this.id).emit('updatePlayerLife', [player.id, player.life]);
+              io.to(this.id).emit('removeBullet', shot.id);
+            }
+          }
         }
-      })
+      }
     }
-
-    io.to(this.id).emit('update', {
-      players: this.players,
-      shots: this.shots,
-      smokes: this.smokes,
-      objects: this.objects
-    });
-
-    this.smokes = [];
   }
 }
 
-/** @type {Room[]} */
-let rooms = [];
-
 const gameLoop = () => {
-  rooms.forEach(room => room.update());
-  setTimeout(gameLoop, 1000 / 30);
+  for (const id in rooms) {
+    rooms[id].update();
+  }
+  setTimeout(gameLoop, 1000 / 60);
 }
 
 gameLoop();
 
 io.on('connection', socket => {
-  let room = rooms.find(e => e.players.length < 4);
+  /** @type {Room} */
+  let room;
+
+  for (const id in rooms) {
+    if (Object.keys(rooms[id].players).length < 4) {
+      room = rooms[id];
+    }
+  }
 
   if (!room) {
     room = new Room();
-    rooms.push(room);
+    rooms[room.id] = room;
   }
 
-  let player = new Player(socket.id);
-  while (room.checkCollisions(player)) {
-    player.respawn();
-  }
-  room.players.push(player);
+  let player = new Player(socket.id, room.id);
+  player.respawn();
+  room.players[socket.id] = player;
+
+  const { players, objects } = room;
   socket.join(room.id);
 
-  let players = room.players;
-  const { shots, smokes, objects } = room;
-  io.to(room.id).emit('join', {
+  socket.emit('join', {
     terrain: room.terrain,
     players,
-    shots,
-    smokes,
+    shots: room.shots,
     objects
   });
 
-  socket.on('move', (data) => { player.keys = data.keys })
+  socket.broadcast.to(room.id).emit('newPlayer', player);
+  socket.on('move', (keys) => { player.move(keys) })
 
   socket.on('disconnect', () => {
-    room.players = room.players.filter(player => player.id != socket.id);
-    if (room.players.length == 0) {
-      rooms = rooms.filter(r => r.id != room.id);
+    delete room.players[socket.id];
+    if (Object.keys(room.players).length == 0) {
+      delete rooms[room.id];
+    } else {
+      socket.broadcast.to(room.id).emit('playerDisconnected', player.id);
     }
-    let players = room.players;
-    const { shots, smokes, objects } = room;
-    socket.to(room.id).emit('update', {
-      players,
-      shots,
-      smokes,
-      objects
-    });
   })
 });
 
